874beaa149d4bd0e6b807dfde09a5ceca98afd76
touron-travel <manivasagam.shanmugamtnj@gmail.com>
2021-06-24 20:43:30 +0530

    Fonst and icons added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useEffect} from 'react';
 import {SafeAreaView, StatusBar, StyleSheet, Text} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
   useEffect(() => {
@@ -9,7 +10,10 @@ const App = () => {
   return (
     <SafeAreaView>
       <StatusBar />
-      <Text>Welcome to React Native!!</Text>
+      <Text style={{fontFamily: 'PlaylistScript', color: '#fff'}}>
+        Welcome to React Native!!
+      </Text>
+      <Icon name="rocket" size={30} color="#900" />
     </SafeAreaView>
   );
 };
